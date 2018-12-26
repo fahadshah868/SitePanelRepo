76a3fa5df3c73f22321ab28642516b53ec5eb1d4
fahadshah868 <fahadshah868@gmail.com>
2018-12-26 15:54:24 +0500

    updations

--- SitePanel/resources/views/pages/offer/addoffer.blade.php
+++ SitePanel/resources/views/pages/offer/addoffer.blade.php
@@ -7,13 +7,13 @@
                 <div class="col-sm-6">
                     <div class="form-field">
                         <div class="form-field-heading">Offer Title</div>
-                        <input type="text" class="form-control" name="offertitle" placeholder="offer"/>
+                        <input type="text" class="form-control" id="offertitle" name="offertitle" placeholder="offer"/>
                     </div>
                 </div>
                 <div class="col-sm-6">
                     <div class="form-field">
-                        <div class="form-field-heading">Offer Type</div>
-                        <select class="form-control" name="offertype">
+                        <div class="form-field-heading">Offer Type By Store</div>
+                        <select class="form-control" id="offertype_bystore" name="offertype_bystore">
                             <option value="">Select Offer Type</option>
                             @foreach($alloffertypes as $offertype)
                             <option value="{{$offertype->title}}">{{$offertype->title}}</option>
@@ -24,9 +24,16 @@
             </div>
             <div class="row">
                 <div class="col-sm-12">
+                    <div class="form-field-checkbox">
+                        <input type="checkbox" id="offercode-checkbox"><span class="form-field-heading">Code Not Required</span>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
                     <div class="form-field">
                         <div class="form-field-heading">Code</div>
-                        <input type="text" class="form-control" name="offercode" placeholder="code">
+                        <input type="text" class="form-control" id="offercode" name="offercode" placeholder="code">
                     </div>
                 </div>
             </div>
@@ -34,7 +41,7 @@
                 <div class="col-sm-12">
                     <div class="form-field">
                         <div class="form-field-heading">Details</div>
-                        <textarea class="form-control" name="offerdetails" placeholder="description"></textarea>
+                        <textarea class="form-control" id="offerdetails" name="offerdetails" placeholder="description"></textarea>
                     </div>
                 </div>
             </div>
@@ -42,9 +49,9 @@
                 <div class="col-sm-6">
                     <div class="form-field">
                         <div class="form-field-heading">Select Store</div>
-                        <select class="multiselectdropdown" name="selectstore" multiple data-live-search="true">
+                        <select class="multiselectdropdown" id="offer_store" name="offer_store" multiple data-live-search="true">
                             @foreach($allstores as $store)
-                            <option value="{{$store->title}}">{{$store->title}}</option>
+                            <option value="{{$store->id}}">{{$store->title}}</option>
                             @endforeach
                         </select>
                     </div>
@@ -52,46 +59,54 @@
                 <div class="col-sm-6">
                     <div class="form-field">
                         <div class="form-field-heading">Select Category</div>
-                        <select class="multiselectdropdown" name="selectcategory" multiple data-live-search="true">
+                        <select class="multiselectdropdown" id="offer_category" name="offer_category" multiple data-live-search="true">
                             @foreach($allcategories as $category)
-                            <option value="{{$category->title}}">{{$category->title}}</option>
+                            <option value="{{$category->id}}">{{$category->title}}</option>
                             @endforeach
                         </select>
                     </div>
                 </div>
             </div>
             <div class="row">
+                <div class="col-sm-6"></div>
                 <div class="col-sm-6">
-                    <div class="form-field">
-                        <div class="form-field-heading">Start Date</div>
-                        <input type="date" name="startdate" class="form-control"/>
-                    </div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="form-field">
-                        <div class="form-field-heading">End Date</div>
-                        <input type="date" name="enddate" class="form-control"/>
+                    <div class="form-field-checkbox">
+                        <input type="checkbox" name="expiry-date-checkbox" id="expiry-date-checkbox"><span class="form-field-heading">Expiry Date Not Required</span>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-sm-6">
                     <div class="form-field">
-                        <div class="form-field-heading">Coupon Type</div>
-                        <select class="form-control" name="coupontype">
+                        <div class="form-field-heading">Starting Date</div>
+                        <input type="date" id="offer_startingdate" name="offer_startingdate" class="form-control"/>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="form-field">
+                        <div class="form-field-heading">Expiry Date</div>
+                        <input type="date" id="offer_expirydate" name="offer_expirydate" class="form-control"/>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="form-field">
+                        <div class="form-field-heading">Offer Type</div>
+                        <select class="form-control" id="offertype" name="offertype">
                             <option value="">Select Type</option>
-                            <option value="Normal">Normal</option>
-                            <option value="Popular">Popular</option>
+                            <option value="regular">Regular</option>
+                            <option value="popular">Popular</option>
                         </select>
                     </div>
                 </div>
                 <div class="col-sm-6">
                     <div class="form-field">
-                        <div class="form-field-heading">Coupon Status</div>
-                        <select class="form-control" name="couponstatus">
+                        <div class="form-field-heading">Offer Status</div>
+                        <select class="form-control" id="offerstatus" name="offerstatus">
                             <option value="">Select Type</option>
-                            <option value="Active">Active</option>
-                            <option value="Deactive">Deactive</option>
+                            <option value="active">Active</option>
+                            <option value="deactive">Deactive</option>
                         </select>
                     </div>
                 </div>
@@ -103,31 +118,47 @@
 <script type="text/javascript" src="{{asset('js/multiselectdropdown.js')}}"></script>
 <script>
     $(document).ready(function(){
+        $("#offercode-checkbox").click(function(){
+            if($("#offercode-checkbox").prop("checked")){
+                $("#offercode").prop('disabled', true);
+            }
+            else{
+                $("#offercode").prop('disabled', false);
+            }
+        });
+        $("#expiry-date-checkbox").click(function(){
+            if($("#expiry-date-checkbox").prop("checked")){
+                $("#offer_expirydate").prop('disabled', true);
+            }
+            else{
+                $("#offer_expirydate").prop('disabled', false);
+            }
+        });
         //validation rules
         $("#addcouponform").validate({
             rules: {
                 offertitle: "required",
-                offertype: "required",
+                offertype_bystore: "required",
                 offercode: "required",
                 offerdetails: "required",
-                selectstore: "required",
-                selectcategory: "required",
-                startdate: "required",
-                enddate: "required",
-                coupontype: "required",
-                couponstatus: "required"
+                offer_store: "required",
+                offer_category: "required",
+                offer_startingdate: "required",
+                offer_expirydate: "required",
+                offertype: "required",
+                offerstatus: "required"
             },
             messages: {
                 offertitle: "please enter offer title",
-                offertype: "please select offer type",
+                offertype_bystore: "please select offer type",
                 offercode: "please enter offer code",
-                offerdetails: "please enter offer description",
-                selectstore: "please select store",
-                selectcategory: "please select category",
-                startdate: "please select starting date",
-                enddate: "please select ending date",
-                coupontype: "please select coupon type",
-                couponstatus: "please select coupon status"
+                offerdetails: "please enter offer details",
+                offer_store: "please select store",
+                offer_category: "please select category",
+                offer_startingdate: "please select starting date",
+                offer_expirydate: "please select expiry date",
+                offertype: "please select offer type",
+                offerstatus: "please select offer status"
             }
         });
     });
