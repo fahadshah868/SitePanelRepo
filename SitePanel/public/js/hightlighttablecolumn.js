0555dac101b8876e5a04a05957afaba678a7c234
fahadshah868 <fahadshah868@gmail.com>
2019-01-31 17:31:59 +0500

    updations

--- SitePanel/public/js/hightlighttablecolumn.js
+++ SitePanel/public/js/hightlighttablecolumn.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+    $(document).mouseup(function (e){
+        var container = $("#tableview tr");
+        if (!container.is(e.target) && container.has(e.target).length === 0){
+            container.removeClass('highlight-row'); 
+        }
+    });
     $("#tablebody tr").click(function(){
         $("#tablebody tr").removeClass('highlight-row');
         $(this).addClass('highlight-row');
--- SitePanel/resources/views/pages/dashboard.blade.php
+++ SitePanel/resources/views/pages/dashboard.blade.php
@@ -21,7 +21,7 @@
     <!-- Bootstrap JS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
     
-    <div class="body-main-content">
+    <div class="body-main-content" id="body-main-content">
         <div class="wrapper">
             <!-- Sidebar  -->
             <nav id="sidebar">
@@ -144,7 +144,7 @@
                             <i class="fas fa-align-left"></i>
                             <span>Toggle Sidebar</span>
                         </button>
-                        <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#topnavbar" aria-controls="topnavbar" aria-expanded="false" aria-label="Toggle navigation">
                             <i class="fas fa-align-justify"></i>
                         </button>
                         <div class="collapse navbar-collapse" id="topnavbar">
@@ -164,7 +164,7 @@
             </div>
         </div>
     </div>
-<script type="text/javascript">
+    <script type="text/javascript">
         $(document).ready(function () {
             //by default active first link and sub-link
             $("#sidebar ul a:first").addClass("activelink");
